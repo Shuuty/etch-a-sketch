c855ddb8b95eb12ae7bf0c58b0cfa2ee7156f76f
Shuuty <157427708+Shuuty@users.noreply.github.com>
2024-03-03 21:19:30 -0300

    Divs now wrap to container

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
 const rangeBar = document.querySelector(`#rangeBar`);
 const output = document.querySelector(`.rangeBar-number`);
 const container = document.querySelector(`.container`);
+const blockContainer = document.querySelector(`.container-block`);
 
-
+//Show Range Bar Values 
 rangeBar.onchange = () => {
     output.innerHTML = rangeBar.value;
+    randomColor()
 }
 
+//Random Color
+
+function randomColor () {
+    let color;
+
+   console.log( Math.floor(Math.random() * 256))
+
+
+
+
+}
+
+
+
+// Create the blocks
 function createDiv() {
-    let columns = rangeBar.value;
-    let row = rangeBar.value;
-    let divs = columns * row;
-    container.innerHTML = ``
+    let divs = rangeBar.value * rangeBar.value;
+    blockContainer.innerHTML = ``
     for(i = 0; i < divs; i++) {
         let newDiv = document.createElement(`div`);
         newDiv.setAttribute(`class`, `newDiv`);
-        container.appendChild(newDiv);
-        newDiv.style.width = (1 / columns * 100) + "%";
+        newDiv.style.width = (696 / rangeBar.value) + "px";
+        newDiv.style.height = (696 / rangeBar.value) + "px";
+        
+        blockContainer.appendChild(newDiv);
+
+
         newDiv.addEventListener(`mouseover`, function(event){
             event.target.style.backgroundColor = `black`;
         })
