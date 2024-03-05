9e640bf9df78abadeac34c9b435d684e6caf9a3c
Shuuty <157427708+Shuuty@users.noreply.github.com>
2024-03-04 21:01:27 -0300

    Buttons are added to change color

--- script.js
+++ script.js
@@ -2,21 +2,25 @@ const rangeBar = document.querySelector(`#rangeBar`);
 const output = document.querySelector(`.rangeBar-number`);
 const container = document.querySelector(`.container`);
 const blockContainer = document.querySelector(`.container-block`);
+const colorBlack = document.querySelector(`#color-black`);
+const colorRaimbow = document.querySelector(`#color-raimbow`);
+const colorWhite = document.querySelector(`#color-white`);
+const clear = document.querySelector(`#clear`);
 
-//Show Range Bar Values 
+//Show Range Bar Values and update createDiv()
 rangeBar.onchange = () => {
     output.innerHTML = rangeBar.value;
     randomColor()
 }
 
-//Random Color
+// Generate a Random Color
 
-function randomColor () {
+function randomColor() {
     let color;
 
-   console.log( Math.floor(Math.random() * 256))
+    color = `rgb(` + Math.floor(Math.random() * 256) + `,` + Math.floor(Math.random() * 256) + `,` + Math.floor(Math.random() * 256) + `)`
 
-
+    return color;
 
 
 }
@@ -25,20 +29,51 @@ function randomColor () {
 
 // Create the blocks
 function createDiv() {
+    let width = blockContainer.clientWidth;
+    let height = blockContainer.clientHeight;
     let divs = rangeBar.value * rangeBar.value;
     blockContainer.innerHTML = ``
     for(i = 0; i < divs; i++) {
         let newDiv = document.createElement(`div`);
         newDiv.setAttribute(`class`, `newDiv`);
-        newDiv.style.width = (696 / rangeBar.value) + "px";
-        newDiv.style.height = (696 / rangeBar.value) + "px";
+        newDiv.style.width = (width / rangeBar.value) + "px";
+        newDiv.style.height = (height / rangeBar.value) + "px";
         
         blockContainer.appendChild(newDiv);
 
 
-        newDiv.addEventListener(`mouseover`, function(event){
-            event.target.style.backgroundColor = `black`;
+        // Change Colors
+        
+        //  Default color
+        newDiv.addEventListener(`mouseover`, () =>{
+             newDiv.style.backgroundColor = `black`;
         })
+        // Random Color button
+        colorRaimbow.addEventListener(`click`, ()=> {
+            newDiv.addEventListener(`mouseover`, ()=>{
+                newDiv.style.backgroundColor = randomColor()
+            })
+        })
+        // Black color Button
+
+        colorBlack.addEventListener(`click`, () => {
+            newDiv.addEventListener(`mouseover`, ()=>{
+                newDiv.style.backgroundColor = `black`
+            })
+
+        })
+        colorWhite.addEventListener(`click`, () => {
+            newDiv.addEventListener(`mouseover`, ()=>{
+                newDiv.style.backgroundColor = `white`
+            })
+
+        })
+        // Clear button
+        clear.addEventListener(`click`, () =>{
+            newDiv.style.backgroundColor = `white`
+        })
+
+
     
     }
 
